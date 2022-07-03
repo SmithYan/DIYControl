902c67f5110563db8439e410348b805e88affe99
冉星 <smithyanrx@163.com>
2022-07-03 17:37:10 +0800

    第二版本提交

--- combobox/src/main/java/com/smithyan/combobox/ComboBox.java
+++ combobox/src/main/java/com/smithyan/combobox/ComboBox.java
@@ -1,11 +1,19 @@
 package com.smithyan.combobox;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 public class ComboBox extends RelativeLayout {
+    EditText ET ;
+    Spinner SP;
+    @SuppressLint("SetTextI18n")
     public ComboBox(Context context) {
         super(context);
-
+        ET = findViewById(R.id.edittext);
+        SP = findViewById(R.id.spinner);
+        ET.setText("hello");
     }
 }
